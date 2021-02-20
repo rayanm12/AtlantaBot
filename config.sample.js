6fa1ebc35bc8af01982cfe6458a2e88e04ed7360
rayanm12 <79092373+rayanm12@users.noreply.github.com>
2021-02-20 12:45:55 +0300

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,10 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "XXXXXXXXXXX",
+	token: "ODEwODc4Mjg2NzgyNjYwNjA4.YCqDQA.0FMn3vjm1E5jSiFWKNEkGyJ1Qik",
 	/* For the support server */
 	support: {
-		id: "XXXXXXXXXXX", // The ID of the support server
-		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+		id: "735481650464686133", // The ID of the support server
+		logs: "log", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
